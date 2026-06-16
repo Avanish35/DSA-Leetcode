e3638bb0e2d378abcddf09012ffdc4f7b5a81822
Avanish Singh Verma <avanishsingh3521@gmail.com>
2026-06-16 23:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (55.43%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    int solve(int[] nums, int i, int[]dp ){
+        if(i>=nums.length){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int rob = nums[i] + solve(nums, i+2, dp);
+        int skip = solve(nums, i+1, dp);
+        return dp[i] = Math.max(rob, skip);
+    }
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            Arrays.fill(dp,-1);
+        }
+        return solve(nums,0, dp);
+    }
+}
