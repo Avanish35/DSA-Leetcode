c858c238ef162ecfa608e43bba060b720a43fe71
Avanish Singh Verma <avanishsingh3521@gmail.com>
2026-07-25 21:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (10.97%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -18,12 +18,22 @@ class Solution {
         if(root == null){
             return null;
         }
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
 
-        invertTree(root.left);
-        invertTree(root.right);
+            if(node.left!=null){
+                queue.add(node.left);
+            }
+            if(node.right!=null){
+                queue.add(node.right);
+            }
+        }
         return root;
     }
 }
